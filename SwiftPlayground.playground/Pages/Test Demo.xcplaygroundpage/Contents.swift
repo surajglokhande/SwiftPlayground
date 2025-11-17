39281960872fbf459adb861f991bfcd70dd10001
surajglokhande <surajglokhande@gmail.com>
2025-11-17 10:42:18 +0530

    - file updated.

--- SwiftPlayground.playground/Pages/Test Demo.xcplaygroundpage/Contents.swift
+++ SwiftPlayground.playground/Pages/Test Demo.xcplaygroundpage/Contents.swift
@@ -227,7 +227,19 @@ func runNestedSyncToDifferentQueue() {
     print("[\(Date())] Main Thread: Overall process finished.")
 }
 
-//runNestedSyncToDifferentQueue()
+runNestedSyncToDifferentQueue()
+/*:
+ Output:
+ [2025-11-17 04:43:50 +0000] Main Thread: Starting overall process.
+ [2025-11-17 04:43:50 +0000] 🔴 Task on backgroundQueue1: Started.
+ [2025-11-17 04:43:50 +0000] Main Thread: Outer task submitted. Main thread is free.
+ [2025-11-17 04:43:50 +0000] 🔴 Task on backgroundQueue1: Submitting inner Task 🔵 synchronously to serialQueueForInnerWork.
+ [2025-11-17 04:43:50 +0000] 🔵 Task on serialQueueForInnerWork: Started.
+ [2025-11-17 04:43:51 +0000] 🔵 Task on serialQueueForInnerWork: Ended.
+ [2025-11-17 04:43:51 +0000] 🔴 Task on backgroundQueue1: Inner task completed. Continuing its own work.
+ [2025-11-17 04:43:52 +0000] 🔴 Task on backgroundQueue1: Ended.
+ [2025-11-17 04:43:53 +0000] Main Thread: Overall process finished.
+ */
 
 struct MonetaryAmount: Equatable { // struct by default promotes value semantics
     let amount: Decimal
